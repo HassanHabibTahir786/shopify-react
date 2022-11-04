2bf868d72e68863331cdccfd75dd0e14556a2c6c
DaanishAli <danish79786@gmail.com>
2022-11-04 18:08:18 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
+import { Box } from '@mui/system';
 import './App.css';
+import GetGrip from './components/GetGrip';
 import Header from './components/Header';
 import Navbar from './components/Navbar'
 import Slider from './components/Slider'
@@ -8,9 +10,13 @@ function App() {
   return (
     <div >
       <Header />
-      <Navbar/>
-      <Slider/>
-   
+      <Navbar />
+      <Slider />
+      <Box mx={3}>
+        <GetGrip />
+
+      </Box>
+
     </div>
   );
 }
---
+++ src/components/GetGrip.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+import { Typography, Box, styled } from '@mui/material'
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+
+import Slider from "react-slick";
+
+import greenCatcher1 from '../images/getGrip/greenCatcher1.webp'
+import greenCatcher2 from '../images/getGrip/greenCatcher2.webp'
+import pinkCatcher1 from '../images/getGrip/pinkCatcher1.webp'
+import pinkCatcher2 from '../images/getGrip/pinkCatcher2.webp'
+import pinkwhiteCatcher1 from '../images/getGrip/pinkwhiteCatcher1.webp'
+import pinkwhiteCatcher2 from '../images/getGrip/pinkwhiteCatcher2.webp'
+import whiteCatcher1 from '../images/getGrip/whiteCatcher1.webp'
+import whiteCatcher2 from '../images/getGrip/whiteCatcher2.webp'
+
+
+const StyledSliderBox = styled(Box)({
+    margin: "0 8px",
+    position: "relative"
+});
+const SliderNewBox = styled(Box)({
+    position: "absolute",
+    right: "-6px",
+    top: "10px",
+    backgroundColor: "black",
+    color: "white",
+
+});
+
+const GetGrip = () => {
+
+    const [green, setGreen] = useState(greenCatcher1);
+    const [white, setWhite] = useState(pinkwhiteCatcher1);
+
+    const settings = {
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                // from 900 to 1024px
+
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            // from 600 to 900px
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true
+                }
+            },
+            {
+                // from 0 to 600px
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            },
+        ]
+    };
+    return (
+        <Box my={2}>
+            <Typography mt={5} mb={2} align={'center'} sx={{ fontSize: "30px" }}>
+                Get a Grip!
+            </Typography>
+
+            <Slider {...settings}>
+                <div >
+                    <StyledSliderBox  >
+                        <SliderNewBox>
+                            <Typography sx={{ padding: '7px 20px' }}>New</Typography>
+
+                        </SliderNewBox>
+
+                        <img src={green} alt="" srcset="" style={{ width: "100%", cursor: "pointer" }} onMouseOver={() => setGreen(greenCatcher2)} onMouseOut={() => setGreen(greenCatcher1)} />
+                        <Typography align="center" sx={{ fontSize: "22px" }}>Tiger's Eye Hair Claw - Green</Typography>
+                        <Typography align="center" sx={{ fontSize: "20px" }}>$12.00</Typography>
+                    </StyledSliderBox>
+                </div>
+
+                <div >
+                    <StyledSliderBox>
+                        <SliderNewBox>
+                            <Typography sx={{ padding: '7px 20px' }}>New</Typography>
+
+                        </SliderNewBox>
+                        <img src={white} alt="" srcset="" style={{ width: "100%" , cursor: "pointer"}} onMouseOver={() => setWhite(pinkwhiteCatcher2)} onMouseOut={() => setWhite(pinkwhiteCatcher1)} />
+                        <Typography align="center" sx={{ fontSize: "22px" }}>Tiger's Eye Hair Claw - Green</Typography>
+                        <Typography align="center" sx={{ fontSize: "20px" }}>$12.00</Typography>
+                    </StyledSliderBox>
+                </div>
+                <div >
+                    <StyledSliderBox>
+                        <SliderNewBox>
+                            <Typography sx={{ padding: '7px 20px' }}>New</Typography>
+
+                        </SliderNewBox>
+                        <img src={pinkCatcher1} alt="" srcset="" style={{ width: "100%" }} />
+                        <Typography align="center" sx={{ fontSize: "22px" }}>Tiger's Eye Hair Claw - Green</Typography>
+                        <Typography align="center" sx={{ fontSize: "20px" }}>$12.00</Typography>
+                    </StyledSliderBox>
+                </div>
+                <div >
+                    <StyledSliderBox>
+                        <img src={whiteCatcher1} alt="" srcset="" style={{ width: "100%" }} />
+                        <Typography align="center" sx={{ fontSize: "22px" }}>Tiger's Eye Hair Claw - Green</Typography>
+                        <Typography align="center" sx={{ fontSize: "20px" }}>$12.00</Typography>
+                    </StyledSliderBox>
+                </div>
+
+
+            </Slider>
+
+        </Box>
+    )
+}
+
+export default GetGrip
